5424694ac2c6494c5ff286b72b290d13fbd426fa
ghwlchlaks <ghwlchlaks@naver.com>
2019-02-12 16:57:38 +0900

    react <=> server socket 색 바꾸기 테스트 성공

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
 import * as testService from "./services/test";
 
 class App extends Component {
   state = {
-    user : null
+    user : null,
+    endpoint: "http://localhost:4000",
+    color: "white"
   }
   componentDidMount() {
     testService.getData().then((user)=>this.setState({user: user.username}));
   }
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+
+    socket.emit('change color', this.state.color);
+  }
+
+  setColor = (color) => {
+    this.setState({color: color})
+  }
+
   render() {
     const {user} = this.state;
+    const socket = socketIOClient(this.state.endpoint);
+
+    socket.on('change color', (color) => {
+      document.body.style.backgroundColor = color;
+    })
     return (
       <div>
-      {user ? <h1>{`Hello ${user}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <div>
+        {user ? <h1>{`Hello ${user}`}</h1> : <h1>Loading.. please wait!</h1>}
+        </div>
+        <div>
+          <button onClick={this.send}>change color</button>
+          <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+          <button id="red" onClick={() => this.setColor('red')}>Red</button>
+        </div>
       </div>
     );
   }
